7680232c2e4d1852dc156bc7c7c1d95353473490
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-08-06 19:35:48 +0900

    [fix] show_layers

--- show_layers.sh
+++ show_layers.sh
@@ -1,3 +1,12 @@
 tree dump
 manifest_json=$(cat dump/manifest.json | jq -r '.[].Config')
 cat dump/$manifest_json | jq '.'
+
+echo "==========================RUN apk updateの差分==========================="
+tar --list -f dump/$(cat dump/manifest.json | jq -r '.[].Layers[1]')
+
+echo "==========================RUN apk add curlの差分========================="
+tar --list -f dump/$(cat dump/manifest.json | jq -r '.[].Layers[2]')
+
+echo "==========================COPY file.txt /etc/file.txtの差分========================="
+tar --list -f dump/$(cat dump/manifest.json | jq -r '.[].Layers[3]')
