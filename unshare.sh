8ef41d4f13cca75199ec5ee18de402771d177002
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-08-07 09:26:03 +0900

    [feature] mount procfs

--- unshare.sh
+++ unshare.sh
@@ -1,15 +1,15 @@
-echo "-i: IPC"
-echo "-m: Mount"
-echo "-n: Network"
-echo "-p: PID"
-echo "-u: UTS"
-echo "-C: Cgroup"
-echo "unshare UTS and change hostname to example"
+echo "unshare options"
+echo "-i: IPC -m: Mount -n: Network -p: PID -u: UTS -C: Cgroup"
+echo "\nunshare UTS and change hostname to example"
 sudo unshare -imnpuC --fork /bin/bash -c "hostname example && hostname"
 echo "\nhost's host is"
 hostname
+
+echo "\nSee namespace is separated"
 sudo lsns
 
+echo "\nhost procfs"
+sudo unshare -imnpuC --fork /bin/bash -c "ls /proc"
 
-
-
+echo "\nmount new procfs"
+sudo unshare -imnpuC --mount-proc --fork /bin/bash -c "ls /proc"
